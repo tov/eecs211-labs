6d9c5c1419f5fba874bc0748e320eb8ab3db2c4c
Wyatt Cook <wcookeverton@gmail.com>
2017-01-23 23:33:26 -0600

    Added few changes to search function, finished up working draft of Lab Worksheet

--- eecs211-lab03/lab3.cpp
+++ eecs211-lab03/lab3.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <vector>
-
+#include <cstdlib>
 using namespace std;
 
 struct Dog{
     string name;
     double age;
     double happiness; //happiness level from 1 to 5
+    string owner;
 };
 
 
@@ -20,24 +21,29 @@ Dog happiestDog(vector<Dog> dogs){
     return dogs[0];
 }
 
-Dog searchForDog(vector<Dog> dogs, string name){
+string findOwner(vector<Dog> dogs, string name){
     // Write your code here
-    return dogs[0];
+    return dogs[0].owner;
 }
 
 int main(){
     cout<<"Thanks for doing Lab 3! \n";
     //Dog Initialization:
-    Dog roger{"Roger", 5.0, 3.0};
-    Dog angus{"Angus", 12.0, 2.4};
-    Dog marie{"Marie", 0.5, 4.0 };
-    Dog samuel{"Samuel", 7.5, 4.5};
-    Dog pierre{"Pierre", 8.5, 3.5};
-    Dog robert{"Robert", 15, 2.7};
-    Dog forrester{"Forrester", 6.2, 1.5};
+    Dog roger{"Roger", 5.0, 3.0, "Brendan"};
+    Dog angus{"Angus", 12.0, 2.4, "Brendan"};
+    Dog marie{"Marie", 0.5, 4.0 , "Brendan"};
+    Dog samuel{"Samuel", 7.5, 4.5, "Brendan"};
+    Dog pierre{"Pierre", 8.5, 3.5, "Matt"};
+    Dog robert{"Robert", 15, 2.7, "Matt"};
+    Dog forrester{"Forrester", 6.2, 1.5, "Matt"};
     //Vector creation:
     vector<Dog> dogs = {roger, angus, marie, samuel, pierre, robert, forrester};
-
+    /*
+      An example foreach loop that will print out the name of each Dog in dogs
+      for (Dog d : dogs){
+        cout<<d.name<<'\n';
+    }
+     */
     return 0;
 
 
