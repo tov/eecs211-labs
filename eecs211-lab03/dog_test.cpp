e5d52761e8e78a8bc257a89a9375471c4f86bebd
Jesse A. Tov <jesse.tov@gmail.com>
2017-01-24 15:50:32 -0600

    Removed redundancies in tests.

--- eecs211-lab03/dog_test.cpp
+++ eecs211-lab03/dog_test.cpp
@@ -4,7 +4,8 @@
 #include "Dog.h"
 using namespace std;
 
-TEST(HappiestDog)
+// Helper for creating a vector of dogs to test on.
+vector<Dog> test_dog_vector()
 {
     Dog roger{"Roger", 5.0, 3.0, "Brendan"};
     Dog angus{"Angus", 12.0, 2.4, "Brendan"};
@@ -16,38 +17,28 @@ TEST(HappiestDog)
 
     // Vector creation:
     vector<Dog> dogs = {roger, angus, marie, samuel, pierre, rebecca, forrester};
+
+    return dogs;
+}
+
+TEST(HappiestDog)
+{
+    vector<Dog> dogs = test_dog_vector();
 
     CHECK_EQUAL("Samuel", happiestDog(dogs).name);
 }
 
 TEST(YoungestDog)
 {
-    Dog roger{"Roger", 5.0, 3.0, "Brendan"};
-    Dog angus{"Angus", 12.0, 2.4, "Brendan"};
-    Dog marie{"Marie", 0.5, 4.0, "Brendan"};
-    Dog samuel{"Samuel", 7.5, 4.5, "Brendan"};
-    Dog pierre{"Pierre", 8.5, 3.5, "Matt"};
-    Dog rebecca{"Rebecca", 15, 2.7, "Matt"};
-    Dog forrester{"Forrester", 6.2, 1.5, "Matt"};
-
-    // Vector creation:
-    vector<Dog> dogs = {roger, angus, marie, samuel, pierre, rebecca, forrester};
+    vector<Dog> dogs = test_dog_vector();
 
     CHECK_EQUAL(0.5, youngestDog(dogs).age);
 }
 
 TEST(FindOwner)
 {
-    Dog roger{"Roger", 5.0, 3.0, "Brendan"};
-    Dog angus{"Angus", 12.0, 2.4, "Brendan"};
-    Dog marie{"Marie", 0.5, 4.0, "Brendan"};
-    Dog samuel{"Samuel", 7.5, 4.5, "Brendan"};
-    Dog pierre{"Pierre", 8.5, 3.5, "Matt"};
-    Dog rebecca{"Rebecca", 15, 2.7, "Matt"};
-    Dog forrester{"Forrester", 6.2, 1.5, "Matt"};
+    vector<Dog> dogs = test_dog_vector();
 
-    // Vector creation:
-    vector<Dog> dogs = {roger, angus, marie, samuel, pierre, rebecca, forrester};
     CHECK_EQUAL("Brendan", findOwner(dogs, "Marie"));
     CHECK_THROW(findOwner(dogs, "Jesse"), runtime_error);
 }
