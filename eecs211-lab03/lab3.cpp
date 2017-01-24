d3e05e2cfb5c8356437578d295a2cf7b3c52a6f9
Jesse A. Tov <jesse.tov@gmail.com>
2017-01-24 01:35:06 -0600

    Got rid of excess space around marginpars; other miscellaneous cleanup.

--- eecs211-lab03/lab3.cpp
+++ eecs211-lab03/lab3.cpp
@@ -1,52 +1,58 @@
 #include <iostream>
 #include <vector>
 #include <cstdlib>
+
 using namespace std;
 
-struct Dog{
+struct Dog
+{
     string name;
     double age;
-    double happiness; //happiness level from 1 to 5
+    double happiness; // happiness level from 1 to 5
     string owner;
 };
 
-
-Dog youngestDog(vector<Dog> dogs){
+Dog youngestDog(vector<Dog> dogs)
+{
     // Write your code here
     return dogs[0];
 }
 
-Dog happiestDog(vector<Dog> dogs){
+Dog happiestDog(vector<Dog> dogs)
+{
     // Write your code here
     return dogs[0];
 }
 
-string findOwner(vector<Dog> dogs, string name){
+string findOwner(vector<Dog> dogs, string name)
+{
     // Write your code here
     return dogs[0].owner;
 }
 
-int main(){
-    cout<<"Thanks for doing Lab 3! \n";
-    //Dog Initialization:
+int main()
+{
+    cout << "Thanks for doing Lab 3!\n";
+
+    // Dog Initialization:
     Dog roger{"Roger", 5.0, 3.0, "Brendan"};
     Dog angus{"Angus", 12.0, 2.4, "Brendan"};
-    Dog marie{"Marie", 0.5, 4.0 , "Brendan"};
+    Dog marie{"Marie", 0.5, 4.0, "Brendan"};
     Dog samuel{"Samuel", 7.5, 4.5, "Brendan"};
     Dog pierre{"Pierre", 8.5, 3.5, "Matt"};
     Dog robert{"Robert", 15, 2.7, "Matt"};
     Dog forrester{"Forrester", 6.2, 1.5, "Matt"};
-    //Vector creation:
+
+    // Vector creation:
     vector<Dog> dogs = {roger, angus, marie, samuel, pierre, robert, forrester};
+
     /*
-      An example foreach loop that will print out the name of each Dog in dogs
-      for (Dog d : dogs){
-        cout<<d.name<<'\n';
-    }
+      An example for-each loop that will print out the name of each Dog in dogs:
+
+        for (Dog d : dogs) {
+            cout << d.name << '\n';
+        }
      */
-    return 0;
-
-
 }
 
     
