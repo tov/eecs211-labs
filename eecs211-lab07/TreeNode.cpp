9384d4e9bf5eb9e5b0f97364d12ae8ce5faf0d31
Wyatt Cook <wcookeverton@gmail.com>
2017-02-22 00:07:50 -0600

    Fixed up TreeNode class

--- eecs211-lab07/TreeNode.cpp
+++ eecs211-lab07/TreeNode.cpp
@@ -3,4 +3,20 @@
 using namespace std;
 
 
+TreeNode::TreeNode(){
+    left_ = nullptr;
+    right_ = nullptr;
+    data_ = 0;
+}
 
+TreeNode::TreeNode(int data){
+    left_ = nullptr;
+    right_ = nullptr;
+    data_ = data;
+}
+
+TreeNode::TreeNode(TreeNode* left, TreeNode* right, int data){
+    left_ = left;
+    right_ = right;
+    data_ = data;
+}
--- eecs211-lab07/lab7.cpp
+++ eecs211-lab07/lab7.cpp
@@ -10,11 +10,15 @@ using namespace std;
 int getlength(int* arr, size_t sizeOf){
     // note that sizeOf will give you the size of the elements in the array
     /// Write code to traverse the array keeping track of how many elements there are.
+    /// And return the number of elements
     /// FILL IN YOUR CODE HERE
 
 }
 
-
+void swapValues(int* ptr1, int* ptr2){
+    /// Write the function here to swap the values at the ptr1 and ptr2
+    /// FILL IN YOUR CODE HERE
+}
 
 int main()
 {
