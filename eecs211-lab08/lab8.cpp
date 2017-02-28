e84b3e379750e414c6d58b60666325b90a391f8d
Wyatt Cook <wcookeverton@gmail.com>
2017-02-28 01:10:28 -0600

    Added lab8.cpp code functions

--- eecs211-lab08/lab8.cpp
+++ eecs211-lab08/lab8.cpp
@@ -2,8 +2,17 @@
 #include <string>
 #include <vector>
 #include <memory>
-
+#include "Vehicle.h"
 using namespace std;
+
+void moveThisFar(Vehicle* v, double distance){
+    /// FILL ME IN
+}
+
+void moveThisLong(Vehicle* v, double time){
+    /// FILL ME IN
+}
+
 
 int main()
 {
