2b079a432c08d074d833a1a6b9f119e4fdec9eb0
Jesse A. Tov <jesse.tov@gmail.com>
2019-01-02 19:46:56 -0600

    Updated lab1 for C (instead of C++).

--- eecs211-lab01/hello.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-    cout << "Hello 211 student!\n";
-}
---
+++ eecs211-lab01/src/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello 211 student!\n");
+}
