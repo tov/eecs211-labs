89d6ecb5d4ee70b2106fcf6888fc534a421f7498
German Espinosa <germanespinosa@gmail.com>
2019-01-21 14:07:00 -0600

    Update eecs211-lab03/src/lab3.c
    fixed style, used argv instead of local, used bool instead of int for is_prefix_of, fixed printfs.

--- eecs211-lab03/src/lab3.c
+++ eecs211-lab03/src/lab3.c
@@ -1,129 +1,131 @@
+#include <stdbool.h>
 #include<stdio.h>
 #define NORMAL 0
 #define UPPERCASE  1
 #define LOWERCASE  2
 
-char *str_chr(const char *s, char c)
+const char *str_chr(const char *s, char c)
 {
-	while(*s && *s!=c) s++;
-	if (*s!=c) return NULL;
-	return s;
+    while(*s && *s != c) s++;
+    if (*s != c) return NULL;
+    return s;
 }
 
-
-int is_prefix_of(const char* haystack, const char* needle)
+bool is_prefix_of(const char* haystack, const char* needle)
 {
-	while(*needle && *needle==*haystack) 
-	{
-		needle++;
-		haystack++;
-	}
-	if (*needle) return 0;
-	return 1;	
+    while(*needle && *needle == *haystack) 
+    {
+        needle++;
+        haystack++;
+    }
+    if (*needle) return true;
+    return false;
 }
 
-char *str_str(const char *haystack, const char *needle)
+const char *str_str(const char *haystack, const char *needle)
 {
-	while (*haystack)
-	{
-		if (is_prefix_of(haystack,needle)) 
-			return haystack;
-		haystack++;
-	}
-	return NULL;
+    while (*haystack)
+    {
+        if (is_prefix_of(haystack,needle)) 
+            return haystack;
+        haystack++;
+    }
+    return NULL;
 }
 
 char to_upper(char s)
 {
-	if (s>='a' && s<='z')
-		return 'A'+(s-'a');
-	return s;
+    if (s >= 'a' && s <= 'z')
+        return 'A'+(s-'a');
+    return s;
 }
 
 char to_lower(char s)
 {
-	if (s>='A' && s<='Z')
-		return 'a'+(s-'A');
-	return s;
+    if (s >= 'A' && s <= 'Z')
+        return 'a'+(s-'A');
+    return s;
 }
-
 
 char* interpolate(const char* format, const char* args[], char * buffer)
 {
-	int modifier=NORMAL;
-	int argc=0;
-	while (*format)
-	{
-		if (*format == '{')
-		{
-			format++;
-			while (*format!='}')
-			{
-				switch (*format++)
-				{
-					case '^':
-					{
-						modifier=UPPERCASE;
-						break;
-					}
-					case 'v':
-					{
-						modifier=LOWERCASE;
-						break;
-					}
-					default:
-					{
-						return NULL;
-					}
-				}
-			}
-			while (*args[argc])
-			{
-				switch (modifier)
-				{
-					case NORMAL:
-					{
-						*buffer=*args[argc];
-					}
-					case UPPERCASE:
-					{
-						*buffer=to_upper(*args[argc]);
-					}
-					case LOWERCASE:
-					{
-						*buffer = to_lower(*args[argc]);
-					}
-				}
-				args[argc]++;
-				buffer++;
-			}
-			argc++;
-		}
-		else
-		{
-			*buffer = *format;
-			buffer++;
-		}
-		format++;
-	}
-	*buffer = *format;
-	return buffer;
+    int modifier = NORMAL;
+    int argc = 0;
+    char *start = buffer;
+    while (*format)
+    {
+        if (*format == '{')
+        {
+            format++;
+            while (*format != '}')
+            {
+                switch (*format++)
+                {
+                    case '^':
+                    {
+                        modifier = UPPERCASE;
+                        break;
+                    }
+                    case 'v':
+                    {
+                        modifier = LOWERCASE;
+                        break;
+                    }
+                    default:
+                    {
+                        return NULL;
+                    }
+                }
+            }
+            while (*args[argc])
+            {
+                switch (modifier)
+                {
+                    case NORMAL:
+                    {
+                        *buffer = *args[argc];
+                        break;
+                    }
+                    case UPPERCASE:
+                    {
+                        *buffer = to_upper(*args[argc]);
+                        break;
+                    }
+                    case LOWERCASE:
+                    {
+                        *buffer = to_lower(*args[argc]);
+                    }
+                }
+                args[argc]++;
+                buffer++;
+            }
+            argc++;
+        }
+        else
+        {
+            *buffer = *format;
+            buffer++;
+        }
+        format++;
+    }
+    *buffer = *format;
+    return start;
 }
-// Your new struct goes here
 
-
-// Your new function goes here
-
-#ifndef USE_TEST_MAIN
-
-int main(){
-    const char *args[3]={"211","IS","great"};
-    printf(str_chr("hello NU\n",' '));
+int main(int argc, const char *argsv[] )
+{
+    printf("%s\n",str_chr("hello NU",'N'));
     printf("HE in HELLO %d, BEE in BYEBYE %d\n",is_prefix_of("HELLO","HE"),is_prefix_of("BEE","BYEBYE"));
-    printf(str_str("WELCOME INTO 211\n","ME"));
-    char buffer[255];
-    printf(interpolate("Doing labs for {} {v} {^}",args,buffer));
+    printf("%s\n",str_str("WELCOME INTO 211","ME"));
+    char buffer[] = "-------------------------------------------------------------\n";
+	if (argc == 4)
+	{
+		printf("interpolation result:%s\n",interpolate("Doing labs for {} {v} {^}",++argsv,buffer));
+	}
+	else
+	{
+		fprintf(stderr, "unexpected number of arguments\n");
+	}
     return 0;
 }
 
-#endif
