b92534cd7d8f32e2dca57d8e7912d1d63aa6534c
Jesse A. Tov <jesse.tov@gmail.com>
2019-02-09 11:06:57 -0600

    Made helper function static.

--- eecs211-lab04/src/model.cpp
+++ eecs211-lab04/src/model.cpp
@@ -10,7 +10,7 @@ void Model::move_right()
     circle_position.x-=10;
 }
 
-double distance(ge211::Position pos1, ge211::Position pos2)
+static double distance(ge211::Position pos1, ge211::Position pos2)
 {
     int dx = pos1.x - pos2.x;
     int dy = pos1.y - pos2.y;
