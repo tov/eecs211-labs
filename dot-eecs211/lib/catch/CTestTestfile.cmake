8b96c8307d93b8c1065dd348674bae743b6484ba
Jesse A. Tov <jesse.tov@gmail.com>
2019-02-18 10:02:12 -0600

    Removed build files I didn't notice before.

--- dot-eecs211/lib/catch/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cs211/eecs211-lab04/.eecs211/lib/catch
-# Build directory: /home/cs211/eecs211-lab04/.eecs211/lib/catch
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
