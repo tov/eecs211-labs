364a59dc24c1154bd7c720834f9e926eda80f15a
SAMHILLPC\Sam Hill <whatinthesam@gmail.com>
2019-02-26 23:56:27 -0600

    removed all cmake build files, added tile_handlers to the cmakelist

--- eecs211-lab06/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cs211/gitroot/eecs211-labs/eecs211-lab06
-# Build directory: /home/cs211/gitroot/eecs211-labs/eecs211-lab06
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Test_game_test "game_test")
-subdirs(".eecs211/lib/catch")
-subdirs(".eecs211/lib/ge211")
