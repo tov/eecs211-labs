efc611ede409eab8e79e2a3912a9c8d0b3f152f8
cs211 <germanespinosa>
2019-03-04 23:04:04 -0800

    modified:   view.cpp

--- eecs211-lab07/src/view.cpp
+++ eecs211-lab07/src/view.cpp
@@ -28,15 +28,21 @@ void View::draw(ge211::Sprite_set& sprites) const
             case Space_object::Material::metal: // spaceship
             {
                 rotation.set_scale(.3);
+                pos.x -=space_ship_.dimensions().width*.3/2;
+                pos.y -=space_ship_.dimensions().height*.3/2;
                 sprites.add_sprite(space_ship_,pos,2,rotation);
             }
                 break;
             case Space_object::Material::rock: // asteroid
+                pos.x -=asteroid_.dimensions().width/2;
+                pos.y -=asteroid_.dimensions().height*.3/2;
                 sprites.add_sprite(asteroid_,pos,0,rotation);
                 //std::cout << pos.x << "," << pos.y << "\n";
                 break;
             case Space_object::Material::light: // light
                 rotation.set_scale(.03);
+                pos.x -=torpedo_.dimensions().width*.03/2;
+                pos.y -=torpedo_.dimensions().height*.03/2;
                 sprites.add_sprite(torpedo_, pos,1,rotation);
                 break;
         }
