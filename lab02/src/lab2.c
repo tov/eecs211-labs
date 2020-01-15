ebdc17e0ddbb59882e49997e6fe67eab2731a2f1
Jesse A. Tov <jesse.tov@gmail.com>
2020-01-14 18:27:30 -0600

    no more test

--- lab02/src/lab2.c
+++ lab02/src/lab2.c
@@ -1,2 +1,35 @@
+#include <stdio.h>
+
+//
+// Struct definitions
+//
+
+struct Apple
+{
+    double weight;
+    char*  variety;
+    char*  color;
+};
+
+//
+// Function declarations
+//
+
+int some_numbers(int);
+
+//
+// Function definition
+//
+
 int main()
-{ }
+{
+    struct Apple delicious = { 0.3, "Red Delicious", "red" };
+    printf("My %s apple is the color %s and weighs %f lbs.\n",
+            delicious.weight, delicious.variety, delicious.color);
+}
+
+int some_numbers(int num)
+{
+    // TODO;
+    return -1;
+}
