73a1671b22ecd0e9923fcc7fec41bd7b84f03fcd
Jesse A. Tov <jesse.tov@gmail.com>
2020-01-14 18:29:38 -0600

    fixed argument order

--- lab02/src/lab2.c
+++ lab02/src/lab2.c
@@ -24,8 +24,8 @@ int some_numbers(int);
 int main()
 {
     struct Apple delicious = { 0.3, "Red Delicious", "red" };
-    printf("My %s apple is the color %s and weighs %f lbs.\n",
-            delicious.weight, delicious.variety, delicious.color);
+    printf("My %s apple is the color %s and weighs %g lbs.\n",
+            delicious.variety, delicious.color, delicious.weight);
 }
 
 int some_numbers(int num)
