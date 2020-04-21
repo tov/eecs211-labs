70ab36264fd048065bda6cbac7fb7bb35a3f7897
Jesse A. Tov <jesse.tov@gmail.com>
2020-04-20 19:04:25 -0500

    Update lab03 for sp20.

--- lab03/src/lab3_funs.c
+++ lab03/src/lab3_funs.c
@@ -1,6 +1,6 @@
 #include <stdbool.h>
 #include <stddef.h>
-#include <ctype.h> // to get tolower and toupper 
+#include <ctype.h> // to get tolower and toupper
 
 const char *str_chr(const char *s, char c)
 {
